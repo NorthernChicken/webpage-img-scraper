88140fda31c42c2f3b431504f2c0ca4840e21625
NorthernChicken <aj.joshuawu@gmail.com>
2024-04-26 17:37:57 -0700

    better error handling, fixes

--- scraper.py
+++ scraper.py
@@ -10,21 +10,24 @@ def download_images(url, folder_path):
     soup = BeautifulSoup(response.text, 'html.parser')
 
     img_tags = soup.find_all('img')
-    print(img_tags)
+    
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
     
     for img in img_tags:
-        img_url = img['src']
-        
-        if not urlparse(img_url).scheme:
-            img_url = urljoin(url, img_url)
-        
-        filename = os.path.join(folder_path, os.path.basename(img_url))
-        
-        urlretrieve(img_url, filename)
-        print(f"Downloaded {filename}")
+        try:
+            img_url = img['src']
+            
+            if not urlparse(img_url).scheme:
+                img_url = urljoin(url, img_url)
+            
+            filename = os.path.join(folder_path, os.path.basename(img_url))
+            
+            urlretrieve(img_url, filename)
+            print(f"Downloaded {filename}")
+        except Exception as e:
+            print(f"Error downloading image: {e}")
 
-url = 'https://picsum.photos/'
+url = str(input("URL: "))
 folder_path = 'downloaded_images'
 download_images(url, folder_path)
